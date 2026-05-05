c658a5a85a664f99563e2bb8d7d052066cd50c2c
berge231 <kabandaberger@gmail.com>
2026-05-05 14:18:59 +0200

    Updated menu

--- script.js
+++ script.js
@@ -471,3 +471,14 @@ function nextSlide() {
 // Initialize everything
 initCarousel();
 renderMenu(allItems);
+
+// Register Service Worker for PWA
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+    .then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(error => {
+      console.log('Service Worker registration failed:', error);
+    });
+}
